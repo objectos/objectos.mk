d89bd86239a4618df810b5de16ab2327f542a482
Marcio Endo <marcio@objectos.com.br>
2023-08-22 20:34:46 -0300

    tools: define common options for the build tools

---
+++ tools.mk
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2023 Objectos Software LTDA.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Tool options
+#
+
+## java home
+ifdef JAVA_HOME
+JAVA_HOME_BIN := $(JAVA_HOME)/bin
+else
+JAVA_HOME_BIN :=
+endif
+
+## java common options
+JAVA := $(JAVA_HOME_BIN)/java
+
+## javac common options
+JAVAC := $(JAVA_HOME_BIN)/javac
+JAVAC += -g
+JAVAC += -Xpkginfo:always
+JAVAC += --release $(JAVA_RELEASE)
+
+## jar common options
+JAR := $(JAVA_HOME_BIN)/jar
+
+## javadoc common options
+JAVADOC := $(JAVA_HOME_BIN)/javadoc
+JAVADOC += -bottom 'Copyright &\#169; 2022&\#x2013;2023 <a href="https://www.objectos.com.br/">Objectos Software LTDA</a>. All rights reserved.'
+JAVADOC += -charset 'UTF-8'
+JAVADOC += -docencoding 'UTF-8'
+JAVADOC += -doctitle '$(GROUP_ID):$(ARTIFACT_ID) $(VERSION) API'
+JAVADOC += -protected
+JAVADOC += -use
+JAVADOC += -version
+JAVADOC += -windowtitle '$(GROUP_ID):$(ARTIFACT_ID) $(VERSION) API'
+
+## curl common options
+CURL := curl
+CURL += --fail
+
+## gpg common options
+GPG := gpg
+
+## sed common options
+SED := sed
