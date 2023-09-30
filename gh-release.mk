1d77568adec247a86fa0db87d16262fcc3ef7037
Marcio Endo <marcio@objectos.com.br>
2023-09-30 13:20:27 -0300

    [#10] allow creating a GitHub release
    Closes #10

--- gh-release.mk
+++ gh-release.mk
@@ -15,9 +15,7 @@
 #
 
 #
-# @name@ GitHub release body:
-#
-# Generates the markdown file that will serve as the GH release body
+# @name@ GitHub release
 #
 
 ## @name@ GitHub API
@@ -40,7 +38,7 @@ endif
 @prefix@GH_MILESTONE_ID = $(shell $(@prefix@GH_MILESTONE_CURLX) | $(@prefix@GH_MILESTONE_JQX))
 
 ## @name@ Issues from GitHub
-@prefix@GH_ISSUES_JSON = $(@prefix@WORK)/github-issues.json
+@prefix@GH_ISSUES_JSON = $(@prefix@WORK)/gh-issues.json
 
 ## @name@ GitHub issues curl command
 @prefix@GH_ISSUES_CURLX = $(CURL)
@@ -49,7 +47,7 @@ endif
 @prefix@GH_ISSUES_CURLX += $(@prefix@GH_ISSUES_JSON)
 
 ##
-@prefix@GH_RELEASE_BODY = $(@prefix@WORK)/github-release.md
+@prefix@GH_RELEASE_BODY = $(@prefix@WORK)/gh-release.md
 
 ## @name@ Filter and format issues
 @prefix@GH_ISSUES_JQX = $(JQ)
@@ -60,9 +58,41 @@ endif
 
 @prefix@gh_issues = $(let LABEL,$1,$(@prefix@GH_ISSUES_JQX))
 
+##
+@prefix@GH_RELEASE_JSON := $(@prefix@WORK)/gh-release.json
+
+## @name@ git tag name to be generated
+@prefix@GH_TAG := $(@prefix@GH_MILESTONE_TITLE)
+
+##
+@prefix@GH_RELEASE_JQX = $(JQ)
+@prefix@GH_RELEASE_JQX += --raw-input
+@prefix@GH_RELEASE_JQX += --slurp
+@prefix@GH_RELEASE_JQX += '. as $$body | {"tag_name":"$(@prefix@GH_TAG)","name":"Release $(@prefix@GH_TAG)","body":$$body,"draft":true,"prerelease":false,"generate_release_notes":false}'
+@prefix@GH_RELEASE_JQX += $(@prefix@GH_RELEASE_BODY)
+
+## 
+@prefix@GH_RELEASE_CURLX = $(CURL)
+@prefix@GH_RELEASE_CURLX += --data-binary "@$(@prefix@GH_RELEASE_JSON)"
+@prefix@GH_RELEASE_CURLX += --header "Accept: application/vnd.github+json"
+@prefix@GH_RELEASE_CURLX += --header "Authorization: Bearer $(GH_TOKEN)"
+@prefix@GH_RELEASE_CURLX += --header "X-GitHub-Api-Version: 2022-11-28"
+@prefix@GH_RELEASE_CURLX += --location
+@prefix@GH_RELEASE_CURLX += --request POST
+@prefix@GH_RELEASE_CURLX +=  $(@prefix@GH_API)/releases
+
+##
+@prefix@GH_RELEASE_MARKER = $(@prefix@WORK)/gh-release-marker 
+
 #
-# @name@ GitHub release body targets
+# @name@ GitHub release targets
 #
+
+$(@prefix@GH_RELEASE_MARKER): $(@prefix@GH_RELEASE_JSON)
+	@$(@prefix@GH_RELEASE_CURLX)
+
+$(@prefix@GH_RELEASE_JSON): $(@prefix@GH_RELEASE_BODY)
+	$(@prefix@GH_RELEASE_JQX) > $(@prefix@GH_RELEASE_JSON) 
 
 $(@prefix@GH_ISSUES_JSON):
 	$(@prefix@GH_ISSUES_CURLX)
