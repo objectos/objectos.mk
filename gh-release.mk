76d0aed2fd6a5fc7f26392ea4b94f72d7a2fb5e5
Marcio Endo <marcio@objectos.com.br>
2024-11-10 14:47:32 -0300

    feat(gh): the gh-release.mk Makefile #22
    Closes #22
    
    $source=5651425434457090ff5ea78e7e0d1732c886de68$

--- gh-release.mk
+++ gh-release.mk
@@ -15,107 +15,117 @@
 #
 
 #
-# @name@ GitHub release
+#  GitHub release
 #
 
-## @name@ GitHub API
-ifndef @prefix@GH_API
-@prefix@GH_API = https://api.github.com/repos/objectos/$(@prefix@ARTIFACT_ID)
+ifndef WORK
+$(error Required common-clean.mk was not included)
 endif
 
-## @name@ GitHub milestone title
-@prefix@GH_MILESTONE_TITLE = v$(@prefix@VERSION)
+## GitHub API
+ifndef GH_API
+GH_API = https://api.github.com/repos/objectos/$(ARTIFACT_ID)
+endif
 
-## @name@ GitHub milestones curl command
-@prefix@GH_MILESTONE_CURLX = $(CURL)
-@prefix@GH_MILESTONE_CURLX += '$(@prefix@GH_API)/milestones'
+## GitHub milestone title
+GH_MILESTONE_TITLE = v$(VERSION)
 
-## @name@ GitHub milestone number parsing
-@prefix@GH_MILESTONE_JQX = $(JQ)
-@prefix@GH_MILESTONE_JQX += '.[] | select(.title == "$(@prefix@GH_MILESTONE_TITLE)") | .number'
+## GitHub milestones curl command
+GH_MILESTONE_CURLX = curl
+GH_MILESTONE_CURLX += '$(GH_API)/milestones'
 
-## @name@ GitHub milestone ID
-@prefix@GH_MILESTONE_ID = $(shell $(@prefix@GH_MILESTONE_CURLX) | $(@prefix@GH_MILESTONE_JQX))
+## GitHub milestone number parsing
+GH_MILESTONE_JQX = jq
+GH_MILESTONE_JQX += '.[] | select(.title == "$(GH_MILESTONE_TITLE)") | .number'
 
-## @name@ Issues from GitHub
-@prefix@GH_ISSUES_JSON = $(@prefix@WORK)/gh-issues.json
+## GitHub milestone ID
+GH_MILESTONE_ID = $(shell $(GH_MILESTONE_CURLX) | $(GH_MILESTONE_JQX))
 
-## @name@ GitHub issues curl command
-@prefix@GH_ISSUES_CURLX = $(CURL)
-@prefix@GH_ISSUES_CURLX += '$(@prefix@GH_API)/issues?milestone=$(@prefix@GH_MILESTONE_ID)&per_page=100&state=all'
-@prefix@GH_ISSUES_CURLX += >
-@prefix@GH_ISSUES_CURLX += $(@prefix@GH_ISSUES_JSON)
+## Issues from GitHub
+GH_ISSUES_JSON = $(WORK)/gh-issues.json
 
-##
-@prefix@GH_RELEASE_BODY = $(@prefix@WORK)/gh-release.md
-
-## @name@ Filter and format issues
-@prefix@GH_ISSUES_JQX = $(JQ)
-@prefix@GH_ISSUES_JQX += --arg LABEL "$(LABEL)"
-@prefix@GH_ISSUES_JQX += --raw-output
-@prefix@GH_ISSUES_JQX += 'sort_by(.number) | [.[] | {number: .number, title: .title, label: .labels[] | select(.name) | .name}] | .[] | select(.label == $$LABEL) | "- \(.title) \#\(.number)"'
-@prefix@GH_ISSUES_JQX += $(@prefix@GH_ISSUES_JSON)
-
-@prefix@gh_issues = $(let LABEL,$1,$(@prefix@GH_ISSUES_JQX))
+## GitHub issues curl command
+GH_ISSUES_CURLX = curl
+GH_ISSUES_CURLX += '$(GH_API)/issues?milestone=$(GH_MILESTONE_ID)&per_page=100&state=all'
+GH_ISSUES_CURLX += >
+GH_ISSUES_CURLX += $(GH_ISSUES_JSON)
 
 ##
-@prefix@GH_RELEASE_JSON := $(@prefix@WORK)/gh-release.json
+GH_RELEASE_BODY = $(WORK)/gh-release.md
 
-## @name@ git tag name to be generated
-@prefix@GH_TAG := $(@prefix@GH_MILESTONE_TITLE)
+## Filter and format issues
+GH_ISSUES_JQX = jq
+GH_ISSUES_JQX += --arg LABEL "$(LABEL)"
+GH_ISSUES_JQX += --raw-output
+GH_ISSUES_JQX += 'sort_by(.number) | [.[] | {number: .number, title: .title, label: .labels[] | select(.name) | .name}] | .[] | select(.label == $$LABEL) | "- \(.title) \#\(.number)"'
+GH_ISSUES_JQX += $(GH_ISSUES_JSON)
+
+gh_issues = $(let LABEL,$1,$(GH_ISSUES_JQX))
 
 ##
-@prefix@GH_RELEASE_JQX = $(JQ)
-@prefix@GH_RELEASE_JQX += --raw-input
-@prefix@GH_RELEASE_JQX += --slurp
-@prefix@GH_RELEASE_JQX += '. as $$body | {"tag_name":"$(@prefix@GH_TAG)","name":"Release $(@prefix@GH_TAG)","body":$$body,"draft":true,"prerelease":false,"generate_release_notes":false}'
-@prefix@GH_RELEASE_JQX += $(@prefix@GH_RELEASE_BODY)
+GH_RELEASE_JSON := $(WORK)/gh-release.json
+
+## git tag name to be generated
+GH_TAG := $(GH_MILESTONE_TITLE)
+
+##
+GH_RELEASE_JQX = jq
+GH_RELEASE_JQX += --raw-input
+GH_RELEASE_JQX += --slurp
+GH_RELEASE_JQX += '. as $$body | {"tag_name":"$(GH_TAG)","name":"Release $(GH_TAG)","body":$$body,"draft":true,"prerelease":false,"generate_release_notes":false}'
+GH_RELEASE_JQX += $(GH_RELEASE_BODY)
 
 ## 
-@prefix@GH_RELEASE_CURLX = $(CURL)
-@prefix@GH_RELEASE_CURLX += --data-binary "@$(@prefix@GH_RELEASE_JSON)"
-@prefix@GH_RELEASE_CURLX += --header "Accept: application/vnd.github+json"
-@prefix@GH_RELEASE_CURLX += --header "Authorization: Bearer $(GH_TOKEN)"
-@prefix@GH_RELEASE_CURLX += --header "X-GitHub-Api-Version: 2022-11-28"
-@prefix@GH_RELEASE_CURLX += --location
-@prefix@GH_RELEASE_CURLX += --request POST
-@prefix@GH_RELEASE_CURLX +=  $(@prefix@GH_API)/releases
+GH_RELEASE_CURLX = curl
+GH_RELEASE_CURLX += --data-binary "@$(GH_RELEASE_JSON)"
+GH_RELEASE_CURLX += --header "Accept: application/vnd.github+json"
+GH_RELEASE_CURLX += --header "Authorization: Bearer $(GH_TOKEN)"
+GH_RELEASE_CURLX += --header "X-GitHub-Api-Version: 2022-11-28"
+GH_RELEASE_CURLX += --location
+GH_RELEASE_CURLX += --request POST
+GH_RELEASE_CURLX +=  $(GH_API)/releases
 
 ##
-@prefix@GH_RELEASE_MARKER = $(@prefix@WORK)/gh-release-marker 
+GH_RELEASE_MARKER = $(WORK)/gh-release-marker 
 
 #
-# @name@ GitHub release targets
+# GitHub release targets
 #
 
-$(@prefix@GH_RELEASE_MARKER): $(@prefix@GH_RELEASE_JSON)
-	@$(@prefix@GH_RELEASE_CURLX)
+.PHONY: gh-release
+gh-release: $(GH_RELEASE_MARKER)
+
+.PHONY: gh-release@clean
+gh-release@clean:
+	rm -f $(GH_RELEASE_MARKER) $(GH_RELEASE_JSON) $(GH_RELEASE_BODY) $(GH_ISSUES_JSON)
+
+$(GH_RELEASE_MARKER): $(GH_RELEASE_JSON)
+	@$(GH_RELEASE_CURLX)
 	touch $@
 
-$(@prefix@GH_RELEASE_JSON): $(@prefix@GH_RELEASE_BODY)
-	$(@prefix@GH_RELEASE_JQX) > $(@prefix@GH_RELEASE_JSON) 
+$(GH_RELEASE_JSON): $(GH_RELEASE_BODY)
+	$(GH_RELEASE_JQX) > $(GH_RELEASE_JSON) 
 
-$(@prefix@GH_ISSUES_JSON):
-	$(@prefix@GH_ISSUES_CURLX)
+$(GH_ISSUES_JSON):
+	$(GH_ISSUES_CURLX)
 
-$(@prefix@GH_RELEASE_BODY): $(@prefix@GH_ISSUES_JSON)
-	echo '## New features' > $(@prefix@GH_RELEASE_BODY)
-	echo '' >> $(@prefix@GH_RELEASE_BODY)
-	$(call @prefix@gh_issues,t:feature) >> $(@prefix@GH_RELEASE_BODY) 
-	echo '' >> $(@prefix@GH_RELEASE_BODY)
-	echo '## Enhancements' >> $(@prefix@GH_RELEASE_BODY)
-	echo '' >> $(@prefix@GH_RELEASE_BODY)
-	$(call @prefix@gh_issues,t:enhancement) >> $(@prefix@GH_RELEASE_BODY) 
-	echo '' >> $(@prefix@GH_RELEASE_BODY)
-	echo '## Bug Fixes' >> $(@prefix@GH_RELEASE_BODY)
-	echo '' >> $(@prefix@GH_RELEASE_BODY)
-	$(call @prefix@gh_issues,t:defect) >> $(@prefix@GH_RELEASE_BODY) 
-	echo '' >> $(@prefix@GH_RELEASE_BODY)
-	echo '## Documentation' >> $(@prefix@GH_RELEASE_BODY)
-	echo '' >> $(@prefix@GH_RELEASE_BODY)
-	$(call @prefix@gh_issues,t:documentation) >> $(@prefix@GH_RELEASE_BODY) 
-	echo '' >> $(@prefix@GH_RELEASE_BODY)
-	echo '## Work' >> $(@prefix@GH_RELEASE_BODY)
-	echo '' >> $(@prefix@GH_RELEASE_BODY)
-	$(call @prefix@gh_issues,t:work) >> $(@prefix@GH_RELEASE_BODY) 
-	
+$(GH_RELEASE_BODY): $(GH_ISSUES_JSON)
+	echo '## New features' > $(GH_RELEASE_BODY)
+	echo '' >> $(GH_RELEASE_BODY)
+	$(call gh_issues,t:feature) >> $(GH_RELEASE_BODY) 
+	echo '' >> $(GH_RELEASE_BODY)
+	echo '## Enhancements' >> $(GH_RELEASE_BODY)
+	echo '' >> $(GH_RELEASE_BODY)
+	$(call gh_issues,t:enhancement) >> $(GH_RELEASE_BODY) 
+	echo '' >> $(GH_RELEASE_BODY)
+	echo '## Bug Fixes' >> $(GH_RELEASE_BODY)
+	echo '' >> $(GH_RELEASE_BODY)
+	$(call gh_issues,t:defect) >> $(GH_RELEASE_BODY) 
+	echo '' >> $(GH_RELEASE_BODY)
+	echo '## Documentation' >> $(GH_RELEASE_BODY)
+	echo '' >> $(GH_RELEASE_BODY)
+	$(call gh_issues,t:documentation) >> $(GH_RELEASE_BODY) 
+	echo '' >> $(GH_RELEASE_BODY)
+	echo '## Work' >> $(GH_RELEASE_BODY)
+	echo '' >> $(GH_RELEASE_BODY)
+	$(call gh_issues,t:work) >> $(GH_RELEASE_BODY) 
